6feebe18fe7a633886660f767612d36865cc9079
papi-gorilla <jindeensadayoshi@gmail.com>
2021-05-28 12:59:06 +0000

    [add] 緯度経度の表示

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,29 @@
 //= require turbolinks
 //= require_tree .
 
+let map
+let geocoder
+
 function initMap(){
-  let map = new google.maps.Map(document.getElementById('map'), {
+  geocoder=new google.maps.Geocoder()
+  map = new google.maps.Map(document.getElementById('map'), {
   center: {lat: -34.397, lng: 150.644},
   zoom: 8
+  });
+}
+
+function codeAddress(){
+
+  let inputAddress = document.getElementById('address').value;
+  geocoder.geocode( { 'address': inputAddress}, function(results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
   });
 }
