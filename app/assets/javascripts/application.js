7f757c0a27cdd21b6846ee38707ff21692923d7c
papi-gorilla <jindeensadayoshi@gmail.com>
2021-05-28 16:59:29 +0000

    [add] gem及びpostsテーブルへの追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,30 +14,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
-let map
-let geocoder
-
-function initMap(){
-  geocoder=new google.maps.Geocoder()
-  map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 8
-  });
-}
-
-function codeAddress(){
-
-  let inputAddress = document.getElementById('address').value;
-  geocoder.geocode( { 'address': inputAddress}, function(results, status) {
-    if (status == 'OK') {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_124230) do
+ActiveRecord::Schema.define(version: 2021_05_28_165300) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
